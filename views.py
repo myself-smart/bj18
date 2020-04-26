d7ceded0a4ca7f6b696e78de3d0d9eadbaa1ca09
myself-smart <1934454250@qq.com>
2020-04-26 09:42:09 +0800

    '重定向index页面'

--- views.py
+++ views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 
 def index(request):
     """index页面视图"""
     return HttpResponse('ok')
+
+
+def login(request):
+    """重定向到index页面"""
+    return redirect("/index")
